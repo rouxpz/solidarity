f566e78e12fd9b8bc844249498d849fa0f336285
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2013-09-07 17:15:17 -0400

    port changes for heroku

--- app.py
+++ app.py
@@ -49,7 +49,9 @@ def index():
 #start the server
 if __name__ == "__main__":
 	app.debug = True
-	app.run()
+
+	port = int(os.environ.get('PORT', 5000)) #locally runs on 5000, heroku assigns own port
+	app.run(host='0.0.0.0', port=port)
 
 
 
