fae7cddc39214078bd3e0ef2658a5501eb0a9458
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2013-09-20 11:05:59 -0400

    added :

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-print(hi)
+print(hi);
 
 api = twitter.Api(consumer_key=os.environ['consumer_key'],
 	consumer_secret=os.environ['consumer_secret'],
